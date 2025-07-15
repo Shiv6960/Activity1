a3e620143e79095134728285184fa3ae05a894be
Shiv6960 <Shivvv6969@gmail.com>
2025-07-15 13:52:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function submit(){
+
+    var fullname=document.getElementById('Name')
+    console.log(Name.value)
+
+    var phonenumber=document.getElementById('Phno')
+    console.log(Phno.value)
+
+    var emailaddress=document.getElementById('Email')
+    console.log(Email.value)
+
+    var Foodpreferance=document.getElementById('Foodpreference')
+    console.log(Foodpreferance.value)
+
+    var Tshirt=document.getElementById('Tshirt')
+    console.log(Tshirt.value)
+
+    var Gender=document.getElementById('Gender')
+    console.log(Gender.value)
+}
